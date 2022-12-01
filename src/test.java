116fdf7187cf648b1d917103537abd5e90487050
YogevBasgtekaer <yogev7854@gmail.com>
2022-12-01 12:45:25 +0200

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[]args){
+        String str1= "-1000";
+        int a=Integer.parseInt(str1); ;
+        System.out.print(a);
+
+
+
+    }
+}
